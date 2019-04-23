3f00b436cb93c224663478e8a4dfb425e91e2625
Ralph Dittrich <dittrich@qualitus.de>
2019-04-23 17:28:57 +0200

    Modified DataCaptureRoutinesInterface - new comments and parameters

--- classes/API/DataCaptureRoutinesInterface.php
+++ classes/API/DataCaptureRoutinesInterface.php
@@ -3,17 +3,116 @@
 
 namespace QU\LERQ\API;
 
+use QU\LERQ\Model\EventModel;
+
 interface DataCaptureRoutinesInterface
 {
+	/**
+	 * Array with TRUE / FALSE values to define which functions should be overwritten
+	 *   Overrides will work only if this function return an array with the functions' key and value TRUE
+	 *
+	 * @example
+	 * return [
+	 *   'collectUserData'   => false,
+	 *   'collectUDFData'    => false,
+	 *   'collectMemberData' => false,
+	 *   'collectLpPeriod'   => true,
+	 *   'collectObjectData' => false,
+	 * ];
+	 *
+	 * @return array
+	 */
 	public function getOverrides(): array;
 
-	public function collectUserData();
+	/**
+	 * Array with collectable user data
+	 *
+	 * expected keys:
+	 *   user_id
+	 *   username
+	 *   firstname
+	 *   lastname
+	 *   title
+	 *   gender
+	 *   email
+	 *   institution
+	 *   street
+	 *   city
+	 *   country
+	 *   phone_office
+	 *   hobby
+	 *   phone_home
+	 *   phone_mobile
+	 *   phone_fax
+	 *   referral_comment
+	 *   matriculation
+	 *   active
+	 *   approval_date
+	 *   agree_date
+	 *   auth_mode
+	 *   ext_account
+	 *   birthday
+	 *
+	 * @param EventModel $event
+	 * @return array
+	 */
+	public function collectUserData(EventModel $event): array;
 
-	public function collectUDFData(): array;
+	/**
+	 * Array with udf data
+	 *
+	 * recommendation:
+	 *   To get the same output, everytime the function is called,
+	 *   you should return all field ids with null values, if the
+	 *   user has no data for this field.
+	 *
+	 * @param EventModel $event
+	 * @return array
+	 */
+	public function collectUDFData(EventModel $event): array;
 
-	public function collectMemberData();
+	/**
+	 * Array with member data
+	 *
+	 * expected keys:
+	 *   role,
+	 *   course_title
+	 *   course_id
+	 *   course_ref_id
+	 *
+	 * @param EventModel $event
+	 * @return array
+	 */
+	public function collectMemberData(EventModel $event): array;
 
-	public function collectLpPeriod(): array;
+	/**
+	 * Array of learning progress period
+	 *   e.g. course start and course end
+	 *
+	 * expected keys:
+	 *   course_start
+	 *   course_end
+	 *
+	 * @param EventModel $event
+	 * @return array
+	 */
+	public function collectLpPeriod(EventModel $event): array;
 
-	public function collectObjectData();
+	/**
+	 * Array of object data
+	 *
+	 * expected keys:
+	 *   id
+	 *   title
+	 *   ref_id
+	 *   link
+	 *   type
+	 *   course_title
+	 *   course_id
+	 *   course_ref_id
+	 *
+	 * @param EventModel $event
+	 * @return array
+	 */
+	public function collectObjectData(EventModel $event): array;
 }

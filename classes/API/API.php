e0539f1f4057f0b2be2fd574e97ab75a7f2fd11a
Ralph Dittrich <dittrich@qualitus.de>
2019-04-23 17:27:23 +0200

    Added API function getCollectionScheme (Mock version)

--- classes/API/API.php
+++ classes/API/API.php
@@ -32,4 +32,66 @@ class API implements Facade
 		$collector = new Collector($filter);
 		return $collector->collect($no_convert);
 	}
+
+	public function getCollectionScheme()
+	{
+		/**
+		 * integer   => integer
+		 * list      => array
+		 * object    => (object) Entry point for sub-object
+		 * string    => string
+		 * timestamp => (string) Timestamp ISO 8601
+		 */
+		return json_encode([
+			'id' => 'integer',
+			'timestamp' => 'timestamp',
+			'event' => 'string',
+			'event_type' => 'string',
+			'progress' => 'string',
+			'assignment' => 'string',
+			'course_start' => 'timestamp',
+			'course_end' => 'timestamp',
+			'user_data' => 'object',
+			'user_data.usr_id' => 'integer',
+			'user_data.username' => 'string',
+			'user_data.firstname' => 'string',
+			'user_data.lastname' => 'string',
+			'user_data.title' => 'string',
+			'user_data.gender' => 'string',
+			'user_data.email' => 'string',
+			'user_data.institution' => 'string',
+			'user_data.street' => 'string',
+			'user_data.city' => 'string',
+			'user_data.country' => 'string',
+			'user_data.phone_office' => 'string',
+			'user_data.hobby' => 'string',
+			'user_data.department' => 'string',
+			'user_data.phone_home' => 'string',
+			'user_data.phone_mobile' => 'string',
+			'user_data.phone_fax' => 'string',
+			'user_data.referral_comment' => 'string',
+			'user_data.matriculation' => 'string',
+			'user_data.active' => 'integer',
+			'user_data.approval_date' => 'timestamp',
+			'user_data.agree_date' => 'timestamp',
+			'user_data.auth_mode' => 'string',
+			'user_data.ext_account' => 'string',
+			'user_data.birthday' => 'timestamp',
+			'user_data.udf_data' => 'list',
+			'obj_data' => 'object',
+			'obj_data.obj_data.id' => 'integer',
+			'obj_data.title' => 'string',
+			'obj_data.ref_id' => 'integer',
+			'obj_data.link' => 'string',
+			'obj_data.type' => 'string',
+			'obj_data.course_title' => 'string',
+			'obj_data.course_id' => 'integer',
+			'obj_data.course_ref_id' => 'integer',
+			'mem_data' => 'object',
+			'mem_data.role' => 'string',
+			'mem_data.course_title' => 'string',
+			'mem_data.course_id' => 'integer',
+			'mem_data.course_ref_id' => 'integer',
+		]);
+	}
 }

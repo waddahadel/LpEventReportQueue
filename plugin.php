eebd840bbaa2d6e7ff1c047a2f54e15a90dbacf5
Waddah Adel Elsir <chirchillw@gmail.com>
2023-12-19 16:05:06 +0100

    Update plugin.php
    just changed the version to 7.999

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = "lpeventreportqueue";
 $version = "1.4.0";
 $ilias_min_version = "5.3.0";
-$ilias_max_version = "6.999";
+$ilias_max_version = "7.999";
 $responsible = "Ralph Dittrich";
 $responsible_mail = "dittrich@qualitus.de";
 $git_path = 'git@git.qualitus.de:ilias-plugins/LpEventReportQueue.git';
